2f90e1f42fea1fa29a2656a8ab359d37516c6b35
SilverBits1001 <54335145+SilverBits1001@users.noreply.github.com>
2025-12-19 09:08:04 -0800

    Include background page in Vite build

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,22 @@
+import { dirname, resolve } from "path";
+import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
   server: {
     cors: {
       origin: "https://www.owlbear.rodeo",
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        background: resolve(__dirname, "background.html"),
+      },
     },
   },
 });
